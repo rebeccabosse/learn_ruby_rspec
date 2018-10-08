7a720348e8d83c50d570d1bea9af6c013e249899
rebeccabosse <rebeccabosse@hotmail.fr>
2018-10-08 20:08:16 +0200

    modif calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(a,b)
+  result=a+b
+end
+
+def subtract(a,b)
+  result=a-b
+end
+
+def sum(tableau)
+  result=tableau.sum
+end
+
+def multiply(a,b)
+  result=a*b
+end
